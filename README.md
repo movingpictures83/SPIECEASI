a8fdc4cf8a53f53772bb43d3dfa4c95e7a511361
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2018-08-17 08:45:11 -0400

    Multiple bug fixes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# SPIECEASI
-PluMA plugin for SPECI-EASI (Kurtz et al 2015)
+# SPEICEASI
+# Language: R
+# Dependency: Requires devtools and SpeicEasi (run install_github("zdk123/SpiecEasi")) 
+# Input: CSV (abundance matrix)
+# Output: CSV (relationships)
+
+PluMA plugin that runs the SPIEC-EASI (Kurtz et al. 2015) method to infer
+ecological relationships.
+
+The plugin accepts as input a CSV file where rows represent samples and columns
+represent community members, and entry (i, j) represents the abundance of member j
+in sample i.
+
+The plugin produces an output of ecological relationships, as another CSV file.
+Both rows and columns represent members, and entry (i, j) is the relationship between
+member i and member j (mutual relationships only, no directed edges).
--- SPIECEASIPlugin.R
+++ SPIECEASIPlugin.R
@@ -1,9 +1,5 @@
 library(SpiecEasi)
 
-#p_value <- 0.01;
-#libs <- c("Hmisc");
-#lapply(libs, require, character.only=T);
-
 input <- function(inputfile) {
   pc <<- read.csv(inputfile, header = TRUE);
 }
@@ -13,20 +9,16 @@ run <- function() {
   cn <<- colnames(pc);
   cn <<- cn[2:length(cn)];
   pc <<- pc[,-1];
-  #pc <<- apply(pc, 1, as.numeric);
-  #pc <<- t(pc);
-  # Used default parameters; you can change as necessary
-  iscorr <<- spiec.easi(pc, method='mb', lambda.min.ratio=0.01, nlambda=20, icov.select.params=list(rep.num=100))
-  correlations <<- symBeta(getOptBeta(iscorr), mode='maxabs')  
-  #correlations <<- rcorr(pc[,], type=c("pearson"));
-  pc <<- as.matrix(correlations);
-  #pc[is.na(pc)] <<- 0;
-  #empty <- c("");
-  #pc[which(correlations$P>p_value)] <<- 0;
+ pc <<- apply(pc, 1, as.numeric);
+ pc <<- t(pc); 
+iscorr <<- spiec.easi(pc, method='mb', lambda.min.ratio=1e-2,
+                            nlambda=20, icov.select.params=list(rep.num=50))
+correlations <<- symBeta(getOptBeta(iscorr), mode='maxabs')
+  results <<- as.matrix(correlations);
 }
 
 output <- function(outputfile) {
-   write.table(pc, file=outputfile, sep=",", append=FALSE, row.names=unlist(cn), col.names=unlist(cn), na="");
+   write.table(results, file=outputfile, sep=",", append=FALSE, row.names=unlist(cn), col.names=unlist(cn), na="");
 }
 
 
